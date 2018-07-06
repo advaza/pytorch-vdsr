47db0b2d96c5e0e6ffc1472341597176fc64605d
Adva Zair <adva@lightricks.com>
2018-07-06 14:54:31 -0400

    save out image.

--- demo.py
+++ demo.py
@@ -5,14 +5,14 @@ from scipy.ndimage import imread
 from PIL import Image
 import numpy as np
 import time, math
-import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description="PyTorch VDSR Demo")
 parser.add_argument("--cuda", action="store_true", help="use cuda?")
 parser.add_argument("--model", default="model/model_epoch_50.pth", type=str, help="model path")
 parser.add_argument("--image", default="butterfly_GT", type=str, help="image name")
-parser.add_argument("--scale", default=4, type=int, help="scale factor, Default: 4")
 parser.add_argument("--gpus", default="0", type=str, help="gpu ids (default: 0)")
+parser.add_argument("--out_path", default=None, type=str, help="Output image path.")
+
 
 def PSNR(pred, gt, shave_border=0):
     height, width = pred.shape[:2]
@@ -44,13 +44,13 @@ if cuda:
 
 model = torch.load(opt.model, map_location=lambda storage, loc: storage)["model"]
 
-im_gt_ycbcr = imread("Set5/" + opt.image + ".bmp", mode="YCbCr")
-im_b_ycbcr = imread("Set5/"+ opt.image + "_scale_"+ str(opt.scale) + ".bmp", mode="YCbCr")
+#im_gt_ycbcr = imread("Set5/" + opt.image + ".bmp", mode="YCbCr")
+im_b_ycbcr = imread(opt.image + "_scale_"+ str(opt.scale) + ".bmp", mode="YCbCr")
     
-im_gt_y = im_gt_ycbcr[:,:,0].astype(float)
+#im_gt_y = im_gt_ycbcr[:,:,0].astype(float)
 im_b_y = im_b_ycbcr[:,:,0].astype(float)
 
-psnr_bicubic = PSNR(im_gt_y, im_b_y,shave_border=opt.scale)
+#psnr_bicubic = PSNR(im_gt_y, im_b_y,shave_border=opt.scale)
 
 im_input = im_b_y/255.
 
@@ -74,27 +74,28 @@ im_h_y = im_h_y * 255.
 im_h_y[im_h_y < 0] = 0
 im_h_y[im_h_y > 255.] = 255.
 
-psnr_predicted = PSNR(im_gt_y, im_h_y[0,:,:], shave_border=opt.scale)
+#psnr_predicted = PSNR(im_gt_y, im_h_y[0,:,:], shave_border=opt.scale)
 
 im_h = colorize(im_h_y[0,:,:], im_b_ycbcr)
-im_gt = Image.fromarray(im_gt_ycbcr, "YCbCr").convert("RGB")
-im_b = Image.fromarray(im_b_ycbcr, "YCbCr").convert("RGB")
+#im_gt = Image.fromarray(im_gt_ycbcr, "YCbCr").convert("RGB")
+#im_b = Image.fromarray(im_b_ycbcr, "YCbCr").convert("RGB")
 
-print("Scale=",opt.scale)
-print("PSNR_predicted=", psnr_predicted)
-print("PSNR_bicubic=", psnr_bicubic)
+# print("Scale=",opt.scale)
+# print("PSNR_predicted=", psnr_predicted)
+# print("PSNR_bicubic=", psnr_bicubic)
 print("It takes {}s for processing".format(elapsed_time))
+im_h.save(opt.out_path)
 
-fig = plt.figure()
-ax = plt.subplot("131")
-ax.imshow(im_gt)
-ax.set_title("GT")
-
-ax = plt.subplot("132")
-ax.imshow(im_b)
-ax.set_title("Input(bicubic)")
-
-ax = plt.subplot("133")
-ax.imshow(im_h)
-ax.set_title("Output(vdsr)")
-plt.show()
+# fig = plt.figure()
+# ax = plt.subplot("131")
+# ax.imshow(im_gt)
+# ax.set_title("GT")
+#
+# ax = plt.subplot("132")
+# ax.imshow(im_b)
+# ax.set_title("Input(bicubic)")
+#
+# ax = plt.subplot("133")
+# ax.imshow(im_h)
+# ax.set_title("Output(vdsr)")
+# plt.show()
